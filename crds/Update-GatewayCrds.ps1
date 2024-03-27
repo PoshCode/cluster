309ebb35b8c3bf451721883e885068abdb4263c6
Joel Bennett <Jaykul@HuddledMasses.org>
2024-03-26 23:36:50 -0400

    Add the gateway CRDs

---
+++ crds/Update-GatewayCrds.ps1
@@ -0,0 +1,13 @@
+param(
+    $Version = "v1.0.0"
+)
+Push-Location $PSScriptRoot
+
+Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml > gateway-networking-$Version-gatewayclasses.yaml
+Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/standard/gateway.networking.k8s.io_gateways.yaml > gateway-networking-$Version-gateways.yaml
+Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml > gateway-networking-$Version-httproutes.yaml
+Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml > gateway-networking-$Version-referencegrants.yaml
+Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/experimental/gateway.networking.k8s.io_grpcroutes.yaml > gateway-networking-$Version-grpcroutes.yaml
+Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml > gateway-networking-$Version-tlsroutes.yaml
+
+Pop-Location
