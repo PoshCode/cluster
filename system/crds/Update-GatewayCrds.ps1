aabe041b5e0a6b5196dd18d63bc7c25da157f32b
Joel Bennett <Jaykul@HuddledMasses.org>
2024-03-30 11:54:30 -0400

    Try fixing a gateway crash

--- system/crds/Update-GatewayCrds.ps1
+++ system/crds/Update-GatewayCrds.ps1
@@ -4,7 +4,7 @@ param(
 Push-Location $PSScriptRoot
 
 Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml > gateway-networking-$Version-gatewayclasses.yaml
-Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/standard/gateway.networking.k8s.io_gateways.yaml > gateway-networking-$Version-gateways.yaml
+Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/experimental/gateway.networking.k8s.io_gateways.yaml > gateway-networking-$Version-gateways.yaml
 Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml > gateway-networking-$Version-httproutes.yaml
 Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml > gateway-networking-$Version-referencegrants.yaml
 Invoke-RestMethod https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/$Version/config/crd/experimental/gateway.networking.k8s.io_grpcroutes.yaml > gateway-networking-$Version-grpcroutes.yaml
